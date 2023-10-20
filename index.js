219a66dc7ff3c2635d64cbd97218ed7a77b8e840
Shanto57575 <shanto57575@gmail.com>
2023-10-20 20:38:06 +0600

    added blog api

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "Unauthorized Access" })
+    }
+
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: "Unauthorized Access" })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hf3bhoa.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -20,7 +38,6 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
-
 
 async function run() {
     try {
@@ -32,10 +49,25 @@ async function run() {
         const blogsCollection = client.db('DoctorsDB').collection('blogs');
         const shopCollection = client.db('DoctorsDB').collection('shop');
         const cartsCollection = client.db('DoctorsDB').collection('carts');
+        const commentsCollection = client.db('DoctorsDB').collection('comments');
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
 
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded?.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'Admin') {
+                return res.status(403).send({ error: true, message: "Forbidden Access!" })
+            }
+            next();
+        }
 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
@@ -45,10 +77,22 @@ async function run() {
             const query = { email: user?.email }
             const isUserExists = await usersCollection.findOne(query)
             if (isUserExists) {
-                return res.status(403).send({ messages: "User ALready exists", error: "User ALready exists" })
+                return res.send({ messages: "User Already exists" })
             }
-            console.log(user);
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.get('/users/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params?.email;
+
+            if (req.decoded?.email !== email) {
+                return res.send({ Admin: false })
+            }
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = { Admin: user?.role === 'Admin' }
             res.send(result);
         })
 
@@ -144,16 +188,33 @@ async function run() {
             res.send(result)
         })
 
+        // blog comments!
+        app.post('/comments', async (req, res) => {
+            const comment = req.body;
+            const result = await commentsCollection.insertOne(comment);
+            res.send(result);
+        })
+
+        app.get('/comments', async (req, res) => {
+            const result = await commentsCollection.find().toArray();
+            res.send(result);
+        })
+
         app.get('/shop', async (req, res) => {
             const result = await shopCollection.find().toArray();
             res.send(result);
         })
 
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
 
             if (!email) {
                 return res.send([])
+            }
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(401).send({ error: true, message: "Forbidden Access" })
             }
 
             const query = { email: email }
@@ -183,7 +244,6 @@ async function run() {
             const result = await cartsCollection.insertOne(data);
             res.send(result);
         })
-
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
