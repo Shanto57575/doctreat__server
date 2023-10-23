43c4ed40b7e0b769c9fb63a1b8c436594b57cf1e
Shanto57575 <shanto57575@gmail.com>
2023-10-24 02:27:21 +0600

    done

--- index.js
+++ index.js
@@ -279,7 +279,7 @@ async function run() {
             res.send({ insertResult, deleteResult })
         })
 
-        app.get('/payments/:email', async (req, res) => {
+        app.get('/payments/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const query = { email: email }
             const result = await paymentCollection.find(query).toArray();
